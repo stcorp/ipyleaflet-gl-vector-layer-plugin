7f1c6adc79d5afbc65d6938d8a2b79ed31e6a243
Mark Dufour <mark.dufour@stcorp.nl>
2022-07-27 14:01:23 +0200

    0.1.8

--- ipyleaflet_gl_vector_layer/_version.py
+++ ipyleaflet_gl_vector_layer/_version.py
@@ -1,5 +1,5 @@
-version_info = (0, 1, 7)
+version_info = (0, 1, 8)
 
 __version__ = '%s.%s.%s' % (version_info[0], version_info[1], version_info[2])
 
-EXTENSION_VERSION = '^0.1.7'
+EXTENSION_VERSION = '^0.1.8'
