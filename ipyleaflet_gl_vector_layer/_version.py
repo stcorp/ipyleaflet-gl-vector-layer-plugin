b9f38149fe04d6e774f88a4532a898097b3654a1
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2022-12-08 15:10:43 +0100

    Change version of python package to 0.1.13

--- ipyleaflet_gl_vector_layer/_version.py
+++ ipyleaflet_gl_vector_layer/_version.py
@@ -1,5 +1,5 @@
-version_info = (0, 1, 8)
+version_info = (0, 1, 13)
 
 __version__ = '%s.%s.%s' % (version_info[0], version_info[1], version_info[2])
 
-EXTENSION_VERSION = '^0.1.8'
+EXTENSION_VERSION = '^0.1.13'
